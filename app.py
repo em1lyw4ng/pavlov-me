ccf677476533497c805345acfc8904b0313817c5
ecwa2020 <84256988+em1lyw4ng@users.noreply.github.com>
2024-01-07 18:48:38 -0500

    ww cooldown

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import cv2
 import dlib
+import time
 import mediapipe as mp
 
 # initialize camera
@@ -14,6 +15,9 @@ predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 # initialize mediapipe hands object
 mp_hands = mp.solutions.hands.Hands()
 
+# for cooldown calculation
+last_detection_time = 0
+
 while True:
     # 🎥
     ret, frame = cap.read()
@@ -26,6 +30,7 @@ while True:
     faces = detector(gray)
     hands = mp_hands.process(rgb)
 
+    # saves detected points of mouth and hand
     mouth_landmarks = []
     hand_landmarks = []
 
@@ -52,16 +57,21 @@ while True:
                 cx, cy = int(landmark.x * w), int(landmark.y * h)
                 hand_landmarks.append((cx, cy))
                 cv2.circle(frame, (cx, cy), 5, (255, 0, 0), cv2.FILLED)
-            
     
-    # check if any hand landmark is in proximity of mouth landmarks
-    for hand_point in hand_landmarks:
-        for mouth_point in mouth_landmarks:
-            # sqrt((x1 - x2)**2 + (y1-y2)**2)
-            distance = ((hand_point[0] - mouth_point[0]) ** 2 + (hand_point[1] - mouth_point[1]) ** 2) ** 0.5
+    current_time = time.time()
+    hands_on = False
 
-            if distance < 50:
-                print("hands off!!")
+    # calculate time elapsed for cooldown
+    if current_time - last_detection_time > 2:
+        # check if any hand landmark is in proximity of mouth landmarks
+        for hand_point in hand_landmarks:
+            for mouth_point in mouth_landmarks:
+                # sqrt((x1 - x2)**2 + (y1-y2)**2)
+                distance = ((hand_point[0] - mouth_point[0]) ** 2 + (hand_point[1] - mouth_point[1]) ** 2) ** 0.5
+                if distance < 50 and not hands_on:
+                    hands_on = True
+                    last_detection_time = current_time
+                    print("hands off!!")
                 
     # display the resulting frame
     cv2.imshow('Frame', frame)
