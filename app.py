aaf6dca1d6a029c45e2ec3c135141a72dbca8340
ecwa2020 <84256988+em1lyw4ng@users.noreply.github.com>
2024-01-07 18:23:06 -0500

    hand detection!

--- app.py
+++ app.py
@@ -1,37 +1,53 @@
 import cv2
 import dlib
+import mediapipe as mp
 
 # initialize camera
 cap = cv2.VideoCapture(0)
 
 # initialize dlib face detector (HOG-based)
+# and initialize face predictor from dataset
+# http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2 
 detector = dlib.get_frontal_face_detector()
-
-# initialize predictor from dataset
-# http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2
 predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat") 
 
+# initialize mediapipe hands object
+mp_hands = mp.solutions.hands.Hands()
+
 while True:
-    # 👁
+    # 🎥
     ret, frame = cap.read()
 
-    # convert frame to grayscale
+    # convert frame to grayscale and rgb
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-    # detect faces
+    # detect faces and hands
     faces = detector(gray)
+    hands = mp_hands.process(rgb)
 
     for face in faces:
-        # extract facial landmarks
+        # extract facial landmarks from detection
         shape = predictor(gray, face)
 
-        # mouth is in coordinates 48-68 
+        # mouth is in landmarks 48-68 
         mouth = shape.parts()[48:68]
 
-        # drawing time
-        for part in mouth:
-            cv2.circle(frame, (part.x, part.y), 2, (0, 255, 0), -1)
+        # iterate over each landmark and draw 
+        for landmark in mouth:
+            cv2.circle(frame, (landmark.x, landmark.y), 2, (0, 255, 0), -1)
 
+    # check if hand is detected
+    if hands.multi_hand_landmarks:
+        # iterate over each detected hand
+        for hand in hands.multi_hand_landmarks:
+            # iterate over each landmark and draw
+            for landmark in hand.landmark:
+                h, w, c = frame.shape
+                # convert normalized position to pixel coordinates in frame
+                cx, cy = int(landmark.x * w), int(landmark.y * h)
+                cv2.circle(frame, (cx, cy), 5, (255, 0, 0), cv2.FILLED)
+    
     # display the resulting frame
     cv2.imshow('Frame', frame)
 
